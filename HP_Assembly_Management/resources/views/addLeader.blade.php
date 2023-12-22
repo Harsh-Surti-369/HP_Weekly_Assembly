b991a730a136fec04297613446c51cba4132ff99
Harsh Surti <surtiharsh1212@gmail.com>
2023-12-22 22:34:40 +0530

    Center form design

--- HP_Assembly_Management/resources/views/addLeader.blade.php
+++ HP_Assembly_Management/resources/views/addLeader.blade.php
@@ -4,53 +4,66 @@
     Create Center
 @endsection
 @section('content')
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
 
-    <!-- <link rel="stylesheet" href="style.css"> -->
-    <style>
-        body{color: #000;overflow-x: hidden;height: 100%;background-image: url("https://i.imgur.com/GMmCQHC.png");background-repeat: no-repeat;background-size: 100% 100%}.card{padding: 30px 40px;margin-top: 60px;margin-bottom: 60px;border: none !important;box-shadow: 0 6px 12px 0 rgba(0,0,0,0.2)}.blue-text{color: #00BCD4}.form-control-label{margin-bottom: 0}input, textarea, button{padding: 8px 15px;border-radius: 5px !important;margin: 5px 0px;box-sizing: border-box;border: 1px solid #ccc;font-size: 18px !important;font-weight: 300}input:focus, textarea:focus{-moz-box-shadow: none !important;-webkit-box-shadow: none !important;box-shadow: none !important;border: 1px solid #00BCD4;outline-width: 0;font-weight: 400}.btn-block{text-transform: uppercase;font-size: 15px !important;font-weight: 400;height: 43px;cursor: pointer}.btn-block:hover{color: #fff !important}button:focus{-moz-box-shadow: none !important;-webkit-box-shadow: none !important;box-shadow: none !important;outline-width: 0}
-    </style>
-</head>
-<body>
-    <div class="container-fluid px-1 py-5 mx-auto">
-    <div class="row d-flex justify-content-center">
-        <div class="col-xl-7 col-lg-8 col-md-9 col-11 text-center">
-            <h3>Spritual Management</h3>
-            <!-- <p class="blue-text">Just answer a few questions<br> so that we can personalize the right experience for you.</p> -->
-            <div class="card">
-                <h5 class="text-center mb-4">Hari Prabhu Assembly</h5>
-                <form class="form-card" onsubmit="event.preventDefault()">
-                    <div class="row justify-content-between text-left">
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Mandar Name<span class="text-danger"> *</span></label> <input type="text" id="fname" name="fname" placeholder="Enter your Mandar name" onblur="validate(1)"> </div>
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Mandar Area<span class="text-danger"> *</span></label> <input type="text" id="lname" name="lname" placeholder="Enter your Mandar Area" onblur="validate(2)"> </div>
+    <body>
+        <div class="container-fluid px-1 py-5 mx-auto">
+            <div class="row justify-content-center">
+                <div class="col-xl-7 col-lg-8 col-md-9 col-11 text-center">
+                    <h3>New Sabha Center</h3>
+                    <!-- <p class="blue-text">Just answer a few questions<br> so that we can personalize the right experience for you.</p> -->
+                    <div class="card">
+                        <h5 class="text-center mb-4">HariPrabodham Assembly Center</h5>
+                        <form class="form-card" onsubmit="event.preventDefault()">
+                            <div class="row justify-content-between text-start">
+                                <div class="col-sm-6 flex-column d-flex">
+                                    <label class="form-label px-3">Center Name<span class="text-danger">*</span></label>
+                                    <input type="text" id="fname" name="fname" class="form-control"
+                                        placeholder="Enter your Center name" onblur="validate(1)">
+                                </div>
+                                <div class="col-sm-6 flex-column d-flex">
+                                    <label class="form-label px-3">Center Area<span class="text-danger">*</span></label>
+                                    <input type="text" id="lname" name="lname" class="form-control"
+                                        placeholder="Enter your Center Area" onblur="validate(2)">
+                                </div>
+                            </div>
+                            <div class="row justify-content-between text-start">
+                                <div class="col-sm-6 flex-column d-flex">
+                                    <label class="form-label px-3">Center Leader Name<span
+                                            class="text-danger">*</span></label>
+                                    <input type="text" id="email" name="email" class="form-control" placeholder=""
+                                        onblur="validate(3)">
+                                </div>
+                                <div class="col-sm-6 flex-column d-flex">
+                                    <label class="form-label px-3">Phone number<span class="text-danger">*</span></label>
+                                    <input type="text" id="mob" name="mob" class="form-control" placeholder=""
+                                        onblur="validate(4)">
+                                </div>
+                            </div>
+                            <div class="row justify-content-between text-start">
+                                <div class="col-sm-6 flex-column d-flex">
+                                    <label class="form-label px-3">Center Email<span class="text-danger">*</span></label>
+                                    <input type="text" id="job" name="job" class="form-control" placeholder=""
+                                        onblur="validate(5)">
+                                </div>
+                            </div>
+                            <div class="row justify-content-between text-start">
+                                <div class="col-12 flex-column d-flex">
+                                    <label class="form-label px-3">Full address<span class="text-danger">*</span></label>
+                                    <input type="text" id="ans" name="ans" class="form-control" placeholder=""
+                                        onblur="validate(6)">
+                                </div>
+                            </div>
+                            <div class="row justify-content-end">
+                                <div class="col-sm-6">
+                                    <button type="submit" class="btn btn-primary btn-block">Create center</button>
+                                </div>
+                            </div>
+                        </form>
                     </div>
-                    <div class="row justify-content-between text-left">
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Mandar Leader Name<span class="text-danger"> *</span></label> <input type="text" id="email" name="email" placeholder="" onblur="validate(3)"> </div>
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Phone number<span class="text-danger"> *</span></label> <input type="text" id="mob" name="mob" placeholder="" onblur="validate(4)"> </div>
-                    </div>
-                    <div class="row justify-content-between text-left">
-                        <div class="form-group col-sm-6 flex-column d-flex"> <label class="form-control-label px-3">Mandar Email<span class="text-danger"> *</span></label> <input type="text" id="job" name="job" placeholder="" onblur="validate(5)"> </div>
-                    </div>
-                    <div class="row justify-content-between text-left">
-                        <div class="form-group col-12 flex-column d-flex"> <label class="form-control-label px-3">What would you be using Flinks for?<span class="text-danger"> *</span></label> <input type="text" id="ans" name="ans" placeholder="" onblur="validate(6)"> </div>
-                    </div>
-                    <div class="row justify-content-end">
-                        <div class="form-group col-sm-6"> <button type="submit" class="btn-block btn-primary">Request a demo</button> </div>
-                    </div>
-                </form>
+                </div>
             </div>
         </div>
-    </div>
-</div>
-</body>
-</html>
+    </body>
+
+    </html>
 @endsection
--- HP_Assembly_Management/resources/views/layouts/zoneLayout.blade.php
+++ HP_Assembly_Management/resources/views/layouts/zoneLayout.blade.php
@@ -7,6 +7,8 @@
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.0.3/css/font-awesome.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js">
     <link rel="stylesheet" href="style.css">
     <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <title>@yield('title')</title>
@@ -18,8 +20,73 @@
         }
 
         body {
-            background: url("https://img.freepik.com/free-vector/classic-vintage-floral-texture-background_1017-32274.jpg", no-repeat fixed center);
+            color: #000;
+            overflow-x: hidden;
+            height: 100%;
+            background-image: url("https://img.freepik.com/free-vector/vintage-ornamental-flowers-background_52683-28040.jpg?w=996&t=st=1703264298~exp=1703264898~hmac=df9f13065e3b345229dc4753d8cdca8be805485674d0e19e15545b5d13a22a9e");
+            background-repeat: no-repeat;
+            background-size: 100% 100%
+        }
 
+        nav {
+            background: rgba(0, 0, 0, 0.5)
+        }
+
+        .card {
+            padding: 30px 40px;
+            margin-top: 60px;
+            margin-bottom: 60px;
+            border: none !important;
+            box-shadow: 0 6px 12px 0 rgba(0, 0, 0, 0.2)
+        }
+
+        .blue-text {
+            color: #00BCD4
+        }
+
+        .form-control-label {
+            margin-bottom: 0
+        }
+
+        input,
+        textarea,
+        button {
+            padding: 8px 15px;
+            border-radius: 5px !important;
+            margin: 5px 0px;
+            box-sizing: border-box;
+            border: 1px solid #ccc;
+            font-size: 18px !important;
+            font-weight: 300
+        }
+
+        input:focus,
+        textarea:focus {
+            -moz-box-shadow: none !important;
+            -webkit-box-shadow: none !important;
+            box-shadow: none !important;
+            border: 1px solid #00BCD4;
+            outline-width: 0;
+            font-weight: 400
+        }
+
+        .btn-block {
+            text-transform: uppercase;
+            font-size: 15px !important;
+            font-weight: 400;
+            height: 43px;
+            cursor: pointer
+        }
+
+        .btn-block:hover {
+            color: #fff !important
+        }
+
+        button:focus {
+            -moz-box-shadow: none !important;
+            -webkit-box-shadow: none !important;
+            box-shadow: none !important;
+            outline-width: 0
         }
     </style>
 </head>
